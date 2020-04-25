aecab4c0c7804a52e53fa02b9d2c10f6b73ac1e4
kevded <kevded@hotmail.com>
2020-04-25 10:42:31 +0200

    Add integration test with database

--- src/test/java/com/example/integrationtestspringkafka/service/ProducerServiceIntegrationTest.java
+++ src/test/java/com/example/integrationtestspringkafka/service/ProducerServiceIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.example.integrationtestspringkafka.service;
 
 import com.example.integrationtestspringkafka.dto.ExampleDTO;
+import com.example.integrationtestspringkafka.entity.ExampleEntity;
+import com.example.integrationtestspringkafka.repository.ExampleRepository;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -17,8 +19,10 @@ import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Map;
 
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
@@ -33,6 +37,9 @@ public class ProducerServiceIntegrationTest {
     @Autowired
     private ProducerService producerService;
 
+    @Autowired
+    private ExampleRepository exampleRepository;
+
     public ExampleDTO mockExampleDTO(String name, String description) {
         ExampleDTO exampleDTO = new ExampleDTO();
         exampleDTO.setDescription(description);
@@ -40,6 +47,9 @@ public class ProducerServiceIntegrationTest {
         return exampleDTO;
     }
 
+    /**
+     * We verify the output in the topic. With an simulated consumer.
+     */
     @Test
     public void itShould_ProduceCorrectExampleDTO_to_TOPIC_EXAMPLE() {
         // GIVEN
@@ -59,5 +69,42 @@ public class ProducerServiceIntegrationTest {
 
         assertEquals("Une description", valueReceived.getDescription());
         assertEquals("Un nom", valueReceived.getName());
+    }
+
+    /**
+     * We verify the output in the topic. But aslo in the database.
+     */
+    @Test
+    public void itShould_ProduceCorrectExampleDTO_to_TOPIC_EXAMPLE_and_should_saveCorrectExampleEntity() {
+        // GIVEN
+        ExampleDTO exampleDTO = mockExampleDTO("Un nom 2", "Une description 2");
+        // simulation consumer
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("group_consumer_test_2", "false", embeddedKafkaBroker);
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        ConsumerFactory cf = new DefaultKafkaConsumerFactory<String, ExampleDTO>(consumerProps, new StringDeserializer(), new JsonDeserializer<>(ExampleDTO.class, false));
+        Consumer<String, ExampleDTO> consumerServiceTest = cf.createConsumer();
+
+        embeddedKafkaBroker.consumeFromAnEmbeddedTopic(consumerServiceTest, TOPIC_EXAMPLE);
+        // WHEN
+        producerService.send(exampleDTO);
+        // THEN verify output in topic
+        ConsumerRecord<String, ExampleDTO> consumerRecordOfExampleDTO = KafkaTestUtils.getSingleRecord(consumerServiceTest, TOPIC_EXAMPLE);
+        ExampleDTO valueReceived = consumerRecordOfExampleDTO.value();
+
+        assertEquals("Une description 2", valueReceived.getDescription());
+        assertEquals("Un nom 2", valueReceived.getName());
+
+        // THEN verify output in databse
+        List<ExampleEntity> exampleEntityList = exampleRepository.findAll();
+        // we must have 1 entity inserted
+        // We cannot predict when the insertion into the database will occur. So we wait until the value is present. Thank to Awaitility.
+        await().untilAsserted(() -> {
+            assertEquals(1, exampleEntityList.size());
+
+            ExampleEntity firstEntity = exampleEntityList.get(0);
+
+            assertEquals(exampleDTO.getDescription(), firstEntity.getDescription());
+            assertEquals(exampleDTO.getName(), firstEntity.getName());
+        });
     }
 }
