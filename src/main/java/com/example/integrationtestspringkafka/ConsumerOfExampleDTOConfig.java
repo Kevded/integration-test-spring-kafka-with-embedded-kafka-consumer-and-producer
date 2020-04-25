d70c96b495ef7c2a03565f1f7988705956c4cad2
kevded <kevded@hotmail.com>
2020-04-25 09:26:59 +0200

    Update

--- src/main/java/com/example/integrationtestspringkafka/ConsumerOfExampleDTOConfig.java
+++ src/main/java/com/example/integrationtestspringkafka/ConsumerOfExampleDTOConfig.java
@@ -1,8 +1,5 @@
 package com.example.integrationtestspringkafka;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +10,9 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableKafka
@@ -31,7 +31,8 @@ public class ConsumerOfExampleDTOConfig {
     public ConsumerFactory<String, ExampleDTO> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer_group_id_eampledto");
+
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(ExampleDTO.class, false));
     }
 
