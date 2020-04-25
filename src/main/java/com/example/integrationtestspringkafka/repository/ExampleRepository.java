f6ab619bd2d86e9723fe6bda95fa81659908ecef
kevded <kevded@hotmail.com>
2020-04-25 10:42:41 +0200

    Update

--- src/main/java/com/example/integrationtestspringkafka/repository/ExampleRepository.java
+++ src/main/java/com/example/integrationtestspringkafka/repository/ExampleRepository.java
@@ -3,6 +3,8 @@ package com.example.integrationtestspringkafka.repository;
 import com.example.integrationtestspringkafka.entity.ExampleEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ExampleRepository extends CrudRepository<ExampleEntity, Long> {
+import java.util.List;
 
+public interface ExampleRepository extends CrudRepository<ExampleEntity, Long> {
+    List<ExampleEntity> findAll();
 }
